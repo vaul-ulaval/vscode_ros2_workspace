fd9d8043c597409f6d7582c985cc58629abb75f0
Alexandre Guénette <alexandre.guenette.1@ulaval.ca>
2024-01-22 14:36:03 +0000

    Add Release as default build type for the build task

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-# Set the default build type
-BUILD_TYPE=Release
 colcon build \
         --symlink-install \
         --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
